e9a812244e2327839ba6a0a760f1dbab21409f7f
GURU1302 <impiyushghosh@gmail.com>
2024-06-29 17:33:24 +0530

    second file

---
+++ second.py
@@ -0,0 +1,60 @@
+#concatination
+# str = "hello"
+# str2 = "world"
+
+# print(str + " " + str2)
+
+#lowercase
+# str="sneha"
+
+# print(str.upper())
+
+#length
+
+# str = "my name is Sneha Agarwal"
+
+# print(len(str))
+
+
+#replace
+
+# str = "My name is Sneha Agarwal"
+
+# str = str.replace("Sneha", "Piyush") #remember it is case sensitive
+# str = str.replace("Agarwal", "Ghosh")
+
+# print(str)
+
+
+#split
+
+# str  = "My name is Saurabh Kumar Singh"
+
+# words = str.split(" ")
+
+# print(words)
+
+# #strip
+
+# str = "       Guru         "
+
+# str = str.strip()
+
+# print(str)
+
+# #finding something
+
+# str = "the tuition is at 5 pm"
+
+# if "tuition" in str:
+#     print("true")
+
+
+#finding difference between / and //
+
+a =10
+b = 2
+
+print(a / b) #returns float value
+print(a // b) #return integer value
+   
