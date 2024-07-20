428aede1b4db0bed2eb34c69af83bc8189313007
GURU1302 <impiyushghosh@gmail.com>
2024-07-21 03:08:24 +0530

    day 6

---
+++ day6.py
@@ -0,0 +1,31 @@
+##OPERATORS
+
+a= 10
+b = 4
+# print(a//b) #floor division 
+# print(a/b) #exact division
+
+# to directly write code in terminal just write python
+#to come out of that control+D
+
+# print(a is b)
+# print(a is not b)
+# print(a == b)
+
+
+# if a is not b:
+#     print("true")
+# else:
+#     print("false")
+
+# a = True #yaad rakhiyo ki True ka T capital hai
+# b = False
+
+# print(int(a))
+# print(b)
+
+
+# print(a is b  or a is not b) #direct or and likh do
+
+# print (a>b and a is b) relational operator
+
