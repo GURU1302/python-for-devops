56db504b0d335013a888587cae814f6c051c7b4d
GURU1302 <impiyushghosh@gmail.com>
2024-07-21 01:10:30 +0530

    day 4

---
+++ day 4.py
@@ -0,0 +1,75 @@
+#code for calculator in python
+
+# a = 10
+# b = 20
+
+# def add():
+#     c = a+b
+#     print(c)
+
+# def sub():
+#     c = a-b
+#     print(c)
+
+# def mul():
+#     c = a*b
+#     print(c)
+
+# def div():
+#     c = a/b
+#     print(c)
+
+
+# add()
+# sub()
+# mul()
+# div()
+
+#readability, reuseability (modularity) and debugging
+
+
+##modules - groups of functions
+
+# import modules_day4 as calci
+
+# # calci.add() # for importing only one functions from a  module
+# calci #for importing the whole module
+
+
+##packages(library) - collection of modules
+
+#packages -> modules -> functions -> logic
+
+# to install python package we go to pypi and then 
+# search for the package and 
+# just use command
+
+# pip install package_name
+
+
+# to see all packages command is : pip list
+
+## create virtual environment
+
+# pip install virtualenv
+# python -m venv project-abc
+# python -m venv project-xyz
+
+# to activate any one virtual environment 
+#source project-abc/bin/activate
+
+#now if we install anything here then it will be inside this environment 
+#and will not be inside the other virtual environment so 
+#basically is creates virtual seperation
+
+# to deactivate : deactivate
+
+
+
+
+
+
+
+
+
+
---
+++ modules_day4.py
@@ -0,0 +1,26 @@
+#code for calculator in python
+
+a = 10
+b = 20
+
+def add():
+    c = a+b
+    print(c)
+
+def sub():
+    c = a-b
+    print(c)
+
+def mul():
+    c = a*b
+    print(c)
+
+def div():
+    c = a/b
+    print(c)
+
+
+add()
+sub()
+mul()
+div()
