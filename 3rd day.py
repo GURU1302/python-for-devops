af7e8dcf997a7cf28256ca669ea1ee12c1df23fd
GURU1302 <impiyushghosh@gmail.com>
2024-07-20 18:17:07 +0530

    day 3 done and dusted

---
+++ 3rd day.py
@@ -0,0 +1,24 @@
+#keywords
+# 1. keywords, 2. data types 3. operators, 4. Logical reasoning
+# python is a dynamically typed language ex: name = "Guru".
+
+# ec2_instance_name = "project-xyz-instance"
+# print(ec2_instance_name)
+
+# a = 10
+# b = 5
+
+# def addition():
+#     print(a+b)
+#     c = 10
+#     print(a+c)
+
+# def substraction():
+#     print(a-b)
+
+
+# addition()
+# substraction()
+
+#naming good practice, always declare variable in lower case and next is if two words are there in your variable name then either use camel casing or use underscore between each word
+# use variable name a little more descriptive
