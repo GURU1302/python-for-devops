5384c13b105c9cced41f9249553753c93242c5a0
GURU1302 <impiyushghosh@gmail.com>
2024-07-21 20:23:44 +0530

    day 10 was super informative

---
+++ day10.py
@@ -0,0 +1,57 @@
+# ##Advanced list and Exceptional Handling
+
+# # question: print all files from the list of folders
+
+# # algo 
+# # step1- read input from the user
+# # step2- for loop, folder-list files
+# # step3- identify module
+# # step 4- to prrint each and every file
+# # step 5 - identify and handle all the errors
+# import os
+# folders = input("please provide folder name with space in between").split()
+
+# for folder in folders:
+# #if user give a wrong file name and a write file name then
+# #that should be handled wisely and should be user friendly
+# #for that we use error handling
+#    try:
+#     files =  os.listdir(folder)
+#    except FileNotFoundError:
+#     print("please provide a valid folder name, no folder name exist with name - " + folder)
+#     break
+#    except PermissionError:
+#     print("no access to folder - " + folder)
+
+#    print("Listing all files of folder -  " + folder)
+#    for file in files:
+#       print(file)
+
+import os
+
+def list_files_in_folder(folder):
+    try:
+        files = os.listdir(folder)
+        return files,None
+    except FileNotFoundError:
+        return None , "Folder not found" + folder
+    except PermissionError:
+        return None, "Access denied for folder" + folder
+
+
+
+def main():
+    folders = input("Enter the names of folders seperated by spaces: ").split()
+
+    for folder in folders:
+         files,error_message = list_files_in_folder(folder)
+         if files:
+            print(f"files in {folder}:")
+            for file in files:
+                print(file)
+         else:
+            print(f"Error in {folder}: {error_message}")
+
+
+if __name__ == "__main__":
+    main()
--- day9.py
+++ day9.py
@@ -1,18 +1,23 @@
 ##LOOPS 
 
 
-# for i in range (10):
-#     print(i+1)
+# # for i in range (0,10,2): for(i= 0;i<10;i+=2)
+#     print(i)
+
+# for i in range(10):
+    # print(i)
 
 # i = 9
 # while i <= 10:
-#     print(i)
-#     i+=1
+#    print(i)
+#    
 
-# list1 = ["yellow","green","brown"]
+list1 = ["yellow","green","brown"]
 
-# for i in list1:
-#     print(i)
+for i in list1:
+    print(i,end=" ")
+
+# print(*list1) #to print all element with space in one line
 
 
 ## loop manipulation statement
